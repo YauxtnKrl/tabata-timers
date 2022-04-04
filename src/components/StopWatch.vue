ef655af25d49218ffbff6be4f50ca8c9f87f12fd
yauxtnkrl <justinccarroll1@gmail.com>
2022-04-03 20:08:15 -0400

    add basic timer component

---
+++ src/components/StopWatch.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>{{ state.timer }}</p>
+  <button @click="startTimer">Start</button>
+  <button @click="stopTimer(state.interval)">Stop</button>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({ timer: 12 }, { timeLimit: 5 }, { interval: "" });
+
+    function startTimer() {
+      state.interval = setInterval(() => {
+        if (this.timer == 0) {
+          clearInterval(this.in);
+        } else {
+          state.timer--;
+        }
+      }, 1000);
+    }
+
+    function stopTimer(interval) {
+      clearInterval(interval);
+    }
+    function convertToTime(seconds) {
+      console.log(seconds);
+    }
+    return {
+      state,
+      startTimer,
+      stopTimer,
+    };
+  },
+};
+</script>
+
+<style></style>
